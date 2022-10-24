ab6528d5fe2b149c12f9193b9e66137d4a2fdb1d
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-10-24 09:10:33 -0700

    finished; add comments and other stuff

--- dcf.py
+++ dcf.py
@@ -2,152 +2,101 @@ import pandas as pd
 import yfinance as yf
 
 class DCF:
- 
-    def __init__(self, symbol, period, rev_growth_rate, tgr):
-        self._symbol = symbol
-        self._period = period
-        self._rev_growth_rate = rev_growth_rate
+
+    def __init__(self, symbol, proj_growth, tgr):
+        self._ticker = yf.Ticker(symbol)
+        # projected growth rates for operating cash flow
+        self._proj_growth = proj_growth
+        # forecasting period
+        self._period = len(self._proj_growth)
+        # terminal growth rate
         self._tgr = tgr
-        self._tckr = yf.Ticker(symbol)
-        self._info = pd.DataFrame.from_dict(self._tckr.info, orient='index', columns=['Value'])
-        # reverse cols (to ascending FYE)
-        self._financials = self._tckr.financials[self._tckr.financials.columns[::-1]]
-        self._balance_sheet = self._tckr.balance_sheet[self._tckr.balance_sheet.columns[::-1]]
-        self._cash_flow = self._tckr.cashflow[self._tckr.cashflow.columns[::-1]]
-        # rounds all figures to 2 decimal places
+
+        self._info = self._ticker.info
+        # reverse col order (ascending FYE)
+        self._financials = self._ticker.financials[self._ticker.financials.columns[::-1]]
+        self._balance_sheet = self._ticker.balance_sheet[self._ticker.balance_sheet.columns[::-1]]
+        self._cash_flow = self._ticker.cashflow[self._ticker.cashflow.columns[::-1]]
+        self._wacc = self.wacc()
+        # round all figures to 2 decimal places
         pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
-    @property
-    def symbol(self):
-        return self._symbol
-    
-    @property
-    def period(self):
-        return self._period
-    @period.setter
-    def period(self, new_period):
-        self._period = new_period
+    def prep(self):
+        op_cf = self._cash_flow.loc['Total Cash From Operating Activities']
+        capex = self._cash_flow.loc['Capital Expenditures']
+        # CapEx as % of operating cash flow
+        pcnts_capex_op_cf = capex.multiply(-1).div(op_cf)
+        avg_pcnt = pcnts_capex_op_cf.mean()
+        df_prep = self._cash_flow.loc[['Total Cash From Operating Activities']]
+        df_prep.loc['CapEx'] = capex.multiply(-1)
+        for i in range(self._period):
+            proj_op_cf = df_prep.iat[0, -1] * (1 + self._proj_growth[i])
+            proj_capex = avg_pcnt * proj_op_cf
+            df_prep[i + 1] = [proj_op_cf, proj_capex]
+        df_prep.loc['Free Cash Flow'] = df_prep.sum()
+        return df_prep
 
-    @property
-    def rev_growth_rate(self):
-        return self._rev_growth_rate
-    @rev_growth_rate.setter
-    def rev_growth_rate(self, new_rate):
-        self._rev_growth_rate = new_rate
-    
-    @property
-    def tax_rate(self):
-        return tax_rate
-    @tax_rate.setter
-    def tax_rate(self, new_rate):
-        tax_rate = new_rate
+    def dcf(self):
+        df_dcf = self.prep()
+        wacc = self._wacc
+        df_dcf.loc['Present Value of FCF'] = 0
+        for i in range(self._period):
+            curr_fcf = df_dcf.at['Free Cash Flow', i + 1] 
+            df_dcf.at['Present Value of FCF', i + 1] = curr_fcf / (1 + wacc)**(i + 1)
+            return df_dcf
 
-    @property
-    def tgr(self):
-        return self._tgr
-    @tgr.setter
-    def tgr(self, new_tgr):
-        self._tgr = new_tgr
+    def share_price(self):
+        df_dcf = self.dcf()
+        wacc = self._wacc
+        last_fcf = df_dcf.at['Free Cash Flow', self._period]
+        tv = (last_fcf * (1 + self._tgr)) / (wacc - self._tgr)
+        pv_tv = tv / (1 + wacc)**(self._period)
+        enterprise_value = df_dcf.loc['Present Value of FCF'].sum() + pv_tv
+        cash = self._balance_sheet.loc['Cash'][-1]
+        debt = self._balance_sheet.loc['Long Term Debt'][-1]
+        equity_value = enterprise_value + cash - debt
+        shares = self._info.get('sharesOutstanding')
+        share_price = equity_value / shares
+        d = [tv, pv_tv, enterprise_value, cash, debt, equity_value, shares, 
+        share_price]
+        df_sp = pd.DataFrame(data=d, columns=['All numbers in dollars'])
+        df_sp.index = ['Terminal Value', 'Present Value of Terminal Value',
+                'Enterprise Value', 'Cash', 'Debt', 'Equity Value', 'Shares',
+                'Implied Share Price']
+        return df_sp
 
-    @property
-    def info(self):
-        return self._info
+    def wacc(self):
+        # treasury yield for risk free rate
+        tnx = yf.Ticker('^TNX')
+        rfr = tnx.info.get('previousClose') * 0.01
+        # beta
+        b = self._info.get('beta')
+        # equity risk premium
+        erp = 0.056
 
-    @property
-    def financials(self):
-        return self._financials
+        # calculate mean tax rate
+        taxes = self._financials.loc['Income Tax Expense'].abs()
+        ebit = self._financials.loc['Ebit']
+        tax_rates = taxes.div(ebit)
+        tc = tax_rates.mean()
 
-    @property
-    def balance_sheet(self):
-        return self._balance_sheet
-    
-    @property
-    def cash_flow(self):
-        return self._cash_flow
+        # calculate cost of equity
+        cost_equity = rfr + b * (erp - rfr)
+        # market value of equity (market capitalization)
+        e = self._info.get('marketCap')
 
+        # calculate cost of debt
+        interests = self._financials.loc['Interest Expense'].multiply(-1)
+        debts = self._balance_sheet.loc['Long Term Debt']
+        int_rates = interests.div(debts)
+        avg_int_rate = int_rates.mean()
+        cost_debt = avg_int_rate * (1 - tc)
+        # market value of debt (most recent debt figure)
+        d = debts[-1]
 
-    def revenue(self, projected):
-        df_revenue = self._financials.loc[['Total Revenue']]
-        if projected is True:
-            for i in range(self._period):
-                # Projecting revenues with corresponding revenue growth rates
-                last_rev = df_revenue.iat[0, -1]
-                next_rev = last_rev * (1 + self._rev_growth_rate[i])
-                df_revenue[i + 1] = [next_rev]
-        return df_revenue
+        # for ratios
+        v = e + d
 
-    def ebitda(self, projected):
-        # EBITDA = EBIT + D&A
-        ebit = self._financials.loc[['Ebit']]
-        depreciation = self._cash_flow.loc[['Depreciation']]
-        df_calc_ebitda = pd.concat([ebit, depreciation])
-        df_calc_ebitda.loc['EBITDA'] = df_calc_ebitda.sum()
-        df_ebitda = df_calc_ebitda.loc[['EBITDA']]
-        if projected is True:
-            # Calculating % of revenue and averaging for projection
-            df_revenue = self.revenue(projected=False)
-            percents_rev = df_ebitda.loc['EBITDA'].div(df_revenue.loc['Total Revenue'])
-            avg_percent_rev = pd.Series.mean(percents_rev)
-            df_revenue = self.revenue(projected=True)
-            for i in range(self._period):
-                projected_ebitda = df_revenue.loc['Total Revenue'].at[i + 1] * avg_percent_rev
-                df_ebitda[i + 1] = [projected_ebitda]
-        return df_ebitda
-
-    def capex(self, projected):
-        df_capex = -self.cash_flow.loc[['Capital Expenditures']]
-        if projected is True:
-            # Calculating % of revenue and averaging for projection
-            df_revenue = self.revenue(projected=False)
-            percents_rev = df_capex.loc['Capital Expenditures'].div(df_revenue.loc['Total Revenue'])
-            avg_percent_rev = pd.Series.mean(percents_rev)
-            df_revenue = self.revenue(projected=True)
-            for i in range(self._period):
-                projected_capex = df_revenue.loc['Total Revenue'].at[i + 1] * avg_percent_rev
-                df_capex[i + 1] = [projected_capex]
-        return df_capex
-    
-    # TODO: actually calculate the change bruh
-    def change_in_nwc(self, projected):
-        # Change in NWC = Total Current Assets - Total Current Liabilities
-        assets = self._balance_sheet.loc[['Total Current Assets']]
-        liabilities = -self._balance_sheet.loc[['Total Current Liabilities']]
-        df_calc_nwc = pd.concat([assets, liabilities])
-        df_calc_nwc.loc['Change in NWC'] = df_calc_nwc.sum()
-        df_nwc = df_calc_nwc.loc[['Change in NWC']]
-        if projected is True:
-            # Calculating % of revenue and averaging for projection
-            df_revenue = self.revenue(projected=False)
-            percents_rev = df_nwc.loc['Change in NWC'].div(df_revenue.loc['Total Revenue'])
-            avg_percent_rev = pd.Series.mean(percents_rev)
-            df_revenue = self.revenue(projected=True)
-            for i in range(self._period):
-                projected_nwc = df_revenue.loc['Total Revenue'].at[i + 1] * avg_percent_rev
-                df_nwc[i + 1] = [projected_nwc]
-        return df_nwc
-
-    # TODO: fix negative tax messing up projected taxes
-    def taxes(self, projected):
-        df_taxes = self._financials.loc[['Income Tax Expense']]
-        if projected is True:
-            # Calculating % of EBITDA and averaging for projection
-            df_ebitda = self.ebitda(projected=False)
-            percents_ebitda = df_taxes.loc['Income Tax Expense'].div(df_ebitda.loc['EBITDA'])
-            avg_percent_ebitda = pd.Series.mean(percents_ebitda)
-            df_ebitda = self.ebitda(projected=True)
-            for i in range(self._period):
-                projected_taxes = df_ebitda.loc['EBITDA'].at[i + 1] * avg_percent_ebitda
-                df_taxes[i + 1] = [projected_taxes]
-        return df_taxes
-    
-    def unlevered_fcf(self):
-        # Unlevered Free Cash Flow = EBITDA - CapEx - Change in NWC - Taxes
-        df_ebitda = self.ebitda(projected=True)
-        df_capex = self.capex(projected=True)
-        df_nwc = self.change_in_nwc(projected=True)
-        df_taxes = self.taxes(projected=True)
-        df_calc_ufcf = pd.concat([df_ebitda, -df_capex, -df_nwc, -df_taxes])
-        df_calc_ufcf.loc['Unlevered FCF'] = df_calc_ufcf.sum()
-        df_ufcf = df_calc_ufcf.loc[['Unlevered FCF']]
-        return df_ufcf
-
+        # equation
+        wacc = (e/v * cost_equity) + (d/v * cost_debt * (1 - tc))
+        return wacc
--- wacc.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import yfinance as yf
-
-class WACC:
-    pass
